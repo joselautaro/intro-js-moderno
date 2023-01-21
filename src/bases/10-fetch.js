77570b6e8a71e6ef7a03a79a8b46c878197c2500
Lautaro Montaña <joselautarom@gmail.com>
2023-01-21 14:13:55 -0300

    Practicamos fetchAPI

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,19 @@
+const apiKey = 'jShAmTnYZCcyXI6enRjtx9kOAVIWPMTn'
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=jShAmTnYZCcyXI6enRjtx9kOAVIWPMTn`)
+
+peticion
+    .then(resp => resp.json())
+    .then(({ data }) => {
+        const { url } = data.images.original;
+        console.log(url)
+
+        const img = document.createElement('img')
+        img.src = url;
+        document.body.append(img)
+
+    })
+    .catch(console.warn)
+
+
+
